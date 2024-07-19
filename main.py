51046f89fe98c39fd35c733fce65a65a23f31a42
skorik-kirill <60388222+skorik-kirill@users.noreply.github.com>
2024-07-19 12:57:35 +0300

    update user agent

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 
-trusted_user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.'
+trusted_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
 bad_guy = 'fishing site'
 
 @app.route('/hello')
